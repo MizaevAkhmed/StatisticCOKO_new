2a8d5a4c78ecbb6816bfef6ba84d8ef30a59049e
Aysha <ayshatandroid31@gmail.com>
2024-03-15 14:50:24 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include_once("setting.php");?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="../css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="project">
+            <div class="wrapper">
+                <div class="container">
+                    <div class="project_dannie">
+                        <input class="projectTestId" id="projectTestId" type="text">
+                        <input class="projectPass" id="projectPass" type="text">
+                    </div>
+                </div>
+            </div>
+    </div>
+
+    <script src="js/script.js"></script>
+    <script src="js/jquery.js"></script>
+
+</body>
+</html>
