659a16f9c054fd13f1c8d2884196e9c486535899
Aysha <ayshatandroid31@gmail.com>
2024-03-26 17:02:28 +0300

    added excel list

--- query.php
+++ query.php
@@ -1,25 +1,24 @@
 <?php 
 include("setting.php");
+require("vendor/autoload.php");
 
-use PhpOffice\PhpSpreadsheet\IOFactory;
+
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-$spreadsheet = IOFactory::load('template.xlsx');
+// $spreadsheet = new Spreadsheet();
+// $activeWorksheet = $spreadsheet->getActiveSheet();
+// $activeWorksheet->setCellValue('A1', 'Hello World !');
 
-$worksheet = $spreadsheet->getActiveSheet();
+// $writer = new Xlsx($spreadsheet);
+// $writer->save('hello world.xlsx');
 
-$worksheet->getCell('A1')->setValue('John');
-$worksheet->getCell('A2')->setValue('Smith');
+$projectTestId = 3198;
+$pass = 7;
 
-$writer = new Xlsx($spreadsheet);
-$writer->save('write.xlsx');
-
-/*if ($_GET['x']) {
-    $projectTestId = 3280;
-    $pass = 7;
-
-    $sql = "SELECT
+$sql_levels = "SELECT
         a.Code,
         a.Name, 
         SUM(CASE WHEN pt.ProjectTestId = $projectTestId THEN 1 ELSE 0 END) as 'Количество учащихся, зарегистрированных в базе',
@@ -37,8 +36,76 @@ $writer->save('write.xlsx');
         GROUP BY a.Code, a.Name 
         ORDER BY a.Code, a.Name";
 
-    $stmt = sqlsrv_query($conn, $sql);
+    $stmt = sqlsrv_query($conn, $sql_levels);
+
+    $sql_subject = "SELECT
+        es.Name
+        FROM
+        EgeSubjects es
+        join ProjectTests pt on
+        es.Code = pt.SubjectCode
+        WHERE
+        pt.Id = $projectTestId";
+
+    $stmt_subject = sqlsrv_query($conn, $sql_subject);
+
+    $spreadsheet = new Spreadsheet();
+    $activeWorksheet = $spreadsheet->getActiveSheet();
+
+    $activeWorksheet->setCellValue('A1', 'Дата');
+    $activeWorksheet->setCellValue('A3', 'Код');
+    $activeWorksheet->setCellValue('A3', 'Код');
+    $activeWorksheet->setCellValue('B3', 'Название');
+    $activeWorksheet->setCellValue('C3', 'Количество учащихся');
+    $activeWorksheet->setCellValue('D3', 'Количество участников');
+    $activeWorksheet->setCellValue('E3', 'Незачет');
+    $activeWorksheet->setCellValue('F3', 'Зачет');
+
+
+    $alignment = new Alignment();
+    $alignment->setHorizontal(Alignment::HORIZONTAL_CENTER);
+    // $activeWorksheet->getStyle('A1:F1')->getAlignment()->applyFromArray($alignment);
+    // dd($activeWorksheet);
+
+
+    if (($stmt === false) || ($stmt_subject === false)) {
+        die(print_r(sqlsrv_errors(), true)); // Handle query execution error
+    }
     
+
+    $row = 3;
+    $count = 1;
+    while ($row_data = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)) {
+        while ($row_data2 = sqlsrv_fetch_array($stmt_subject, SQLSRV_FETCH_ASSOC)){
+        $activeWorksheet->setCellValue('A' . 1, 'Дата');
+        $activeWorksheet->setCellValue('A' . 2, $row_data2['Name']);
+        }
+        $activeWorksheet->setCellValue('A' . $row, $count);
+        $activeWorksheet->setCellValue('B' . $row, $row_data['Name']);
+        $activeWorksheet->setCellValue('C' . $row, $row_data['Количество учащихся, зарегистрированных в базе']);
+        $activeWorksheet->setCellValue('D' . $row, $row_data['Количество участников диагностики']);
+        $activeWorksheet->setCellValue('E' . $row, $row_data['Незачет']/$row_data['Количество участников диагностики']);
+        $activeWorksheet->getStyle('E' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_PERCENTAGE);
+        $activeWorksheet->setCellValue('F' . $row, $row_data['Зачет']/$row_data['Количество участников диагностики']);
+        $activeWorksheet->getStyle('F' . $row)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_PERCENTAGE);
+        $row++;
+        $count++;
+    }
+    $activeWorksheet->setCellValue('A' . 22, $count++);
+    $activeWorksheet->setCellValue('B' . 22, 'ЧР');
+    $activeWorksheet->setCellValue('C' . 22, "=sum(C3:C21)");
+    $activeWorksheet->setCellValue('D' . 22, "=sum(D3:D21)");
+    $activeWorksheet->setCellValue('D' . 22, "=sum(D3:D21)");
+
+
+    sqlsrv_free_stmt($stmt); // Free the statement resources
+
+    sqlsrv_close($conn); // Close the connection
+
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('output.xlsx');
+
+    /*
     if ($stmt === false) {
         die(print_r(sqlsrv_errors(), true));
     }
