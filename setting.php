bda1a727d4aaeb65693bb393bcd20fcd060b8637
Akhmed Mizaev <ahmadisg99@gmail.com>
2024-04-05 17:21:05 +0300

    first

---
+++ setting.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "192.168.88.150";
+$connectionOptions = array(
+    "Database" => "coko",
+    "Uid" => "ape",
+    "PWD" => "eUrR*Vks1Nd2A8"
+);
+
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn === false) {
+    die(FormatErrors(sqlsrv_errors()));
+}
+
+var_dump($conn);
